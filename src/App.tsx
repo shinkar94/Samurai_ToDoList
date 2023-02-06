2c66ae08076e1213a30ac7aa9c6339314c5ac132
shinkar94 <romka94shinkarenko@gmail.com>
2023-02-07 02:15:21 +0300

    lesson 3 dop

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,35 @@
 import React, {useState} from 'react';
 import './App.css';
 import {TaskType, TodoList} from "./TodoList";
+import {v1} from "uuid";
 
 const hat1 = 'What to learn-1';
 export type FilterValueType = "All" | "Completed" | "Active";
 
 function App() {
     let [tasks, setTask] = useState<Array<TaskType>>([
-        { id: 1, title: "HTML&CSS", isDone: true },
-        { id: 2, title: "JS", isDone: true },
-        { id: 3, title: "ReactJS", isDone: false }
+        { id: v1(), title: "HTML&CSS", isDone: true },
+        { id: v1(), title: "JS", isDone: true },
+        { id: v1(), title: "ReactJS", isDone: false }
     ]);
 
 
-    function removeTask(id: number){
-        let filteredTasks = tasks.filter( el => id !== el.id)
-        setTask(filteredTasks)
+    function removeTask(id: string){
+        setTask(tasks.filter( el => id !== el.id))
+    }
+
+    function addTasks(title:string){
+        let newTask = {id: v1(), title:title, isDone: false}
+        let newTasks = [newTask, ...tasks];
+        setTask(newTasks)
     }
 
     return (
         <div className="App">
             <TodoList hat1={hat1}
                       tasks={tasks}
-                      removeTask={removeTask}/>
+                      removeTask={removeTask}
+                      addTasks={addTasks}/>
         </div>
     );
 }
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,23 +1,41 @@
-import React, {useState} from "react";
+import React, {ChangeEvent, KeyboardEvent, useState} from "react";
 import {FilterValueType} from "./App";
 
 type PropsType={
     hat1?: string
     hat2?: string
     tasks: Array<TaskType>
-    removeTask: (id: number) => void
+    removeTask: (id: string) => void
+    addTasks: (title: string)=>void
 }
 export type TaskType={
-    id: number,
+    id: string,
     title: string,
     isDone: boolean
 }
 
 export const TodoList = (props:PropsType) =>{
+
+    let [filter, setFilter] = useState<FilterValueType>("All")
+    const [newTasksTitle, setNewTitle] = useState("")
+
     function changeFilter(value: FilterValueType){
         setFilter(value)
     }
-    let [filter, setFilter] = useState<FilterValueType>("All")
+    const onChangerHandler = (e: ChangeEvent<HTMLInputElement>) =>{
+        setNewTitle(e.currentTarget.value)
+    }
+    const onKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
+        if(e.charCode === 13){
+            props.addTasks(newTasksTitle)
+            setNewTitle("")
+        }
+    }
+    const addTasks = () =>{
+        props.addTasks(newTasksTitle)
+        setNewTitle("")
+    }
+
     let tasksForTodoList = props.tasks;
     if(filter === "Completed"){
         tasksForTodoList = props.tasks.filter( t => t.isDone === true);
@@ -30,14 +48,23 @@ export const TodoList = (props:PropsType) =>{
             <h3>{props.hat1}</h3>
             <h3>{props.hat2}</h3>
             <div>
-                <input/>
-                <button>+</button>
+                <input
+                    onChange={onChangerHandler}
+                    onKeyPress={onKeyPress}
+                    value={newTasksTitle}
+                />
+                <button onClick={addTasks}>+</button>
             </div>
             <ul>
                 {
-                    tasksForTodoList.map((el)=>{
+                    tasksForTodoList.map(el=>{
+                        const onRemove = () => props.removeTask(el.id)
                         return(
-                            <li key={el.id}><button onClick={()=>{props.removeTask(el.id)}}>X</button><input type="checkbox" checked={el.isDone}/> <span>{el.title}</span></li>
+                            <li key={el.id}>
+                                <button onClick={onRemove}>X</button>
+                                <input type="checkbox" checked={el.isDone}/>
+                                <span>{el.title}</span>
+                            </li>
                         )
                     })
                 }
